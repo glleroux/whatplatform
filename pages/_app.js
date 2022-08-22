560d2a35f309d744359d763156a41a88794f4605
Rory Buckle <rorytbuckle@gmail.com>
2022-08-22 19:08:54 +0100

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import { usePanelbear } from '@panelbear/panelbear-nextjs';
 
 function MyApp({ Component, pageProps }) {
+
+  usePanelbear('9XlXEMIpq8X');
+
   return <Component {...pageProps} />
 }
 
